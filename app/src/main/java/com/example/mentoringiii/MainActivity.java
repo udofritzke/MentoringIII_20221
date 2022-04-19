e89b8a6e2bcca7e0a60a410f16ab7fe61265780c
udofritzke <udo.fritzke@gmail.com>
2022-04-19 15:06:57 -0300

    teste 2

--- app/src/main/java/com/example/mentoringiii/MainActivity.java
+++ app/src/main/java/com/example/mentoringiii/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+// comentário 1
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
