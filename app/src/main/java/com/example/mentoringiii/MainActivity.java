931c222b71ee6a409f21eb295caa2e3bdb88ff63
udofritzke <udo.fritzke@gmail.com>
2022-04-22 10:58:02 -0300

    tercceiro comentario

--- app/src/main/java/com/example/mentoringiii/MainActivity.java
+++ app/src/main/java/com/example/mentoringiii/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 // comentário 1
 // comentário 2
+// comentário 3
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
