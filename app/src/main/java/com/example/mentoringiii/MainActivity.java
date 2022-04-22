efaad7d72c73bc059cb307d7a35f78bb5c1ec1f9
udofritzke <udo.fritzke@gmail.com>
2022-04-22 11:25:48 -0300

    tercceiro comentario

--- app/src/main/java/com/example/mentoringiii/MainActivity.java
+++ app/src/main/java/com/example/mentoringiii/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mentoringiii;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 // comentário 1
@@ -13,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("teste", "Olá teste");
     }
 }
